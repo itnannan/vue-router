640d60f3e4a54733b385cd7bab41e77591686ba8
tangpeinan <tangpn@zgtx.local>
2017-04-12 10:46:07 +0800

    vue-router

---
+++ src/component/firstcomponent.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="firstvue">
+		<h1>haha</h1>
+		<a>write by{{author}}</a>
+	</div>
+</template>
+<script>
+	export default{
+		data (){
+			return {
+				author: "haha"
+			}
+		}
+	}
+</script>
+<style>
+	
+</style>
---
+++ src/component/secondcomponent.vue
@@ -0,0 +1,40 @@
+<template>
+	<div id="secondcomponent">
+		<h1>第二页</h1>
+		<a>write by{{author}}</a>
+		<p> 感谢 <a href="https://github.com/showonne">showonne</a>大神的技术指导</p>
+		<div class="box-card">
+		      <div slot="header" class="clearfix">
+		        <h1 style="line-height: 36px; color: #20A0FF">豆瓣电影排行榜</h1>
+		      </div>
+		      <div v-for="article in articles" class="text item">
+		        {{article.title}}
+		      </div>
+		</div>
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return {
+				author :'干得漂亮',
+				articles:[],
+			}
+		},
+		mounted:function(){
+		    this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10',{},{
+		      headers:{
+
+		      },
+		      emulateJSON:true
+		    }).then(function(response){
+		    	this.articles = response.data.subjects
+		      console.log(response);
+		    })
+		  }
+	}
+
+</script>
+<style>
+	
+</style>
